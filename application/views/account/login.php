b9790d3cb715893b4d7e84b6a5fbdde24b280966
Dmitry Poliuha <dmitry.polyuha@gmail.com>
2018-05-26 00:48:07 +0300

    add views

---
+++ application/views/account/login.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dmitry
+ * Date: 25.05.2018
+ * Time: 23:42
+ */
+?>
+
+<h2>login page</h2>
+<form>
+    <p>login</p>
+    <p><input type="text"></p>
+    <p>password</p>
+    <p><input type="text"></p>
+    <b><button>log in</button></b>
+
+</form>
---
+++ application/views/main/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dmitry
+ * Date: 25.05.2018
+ * Time: 23:41
+ */
+?>
+
+<p>main page</p>
