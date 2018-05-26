bc90b8d65f39b68147bfda6629af49e9be9db113
Dmitry Poliuha <dmitry.polyuha@gmail.com>
2018-05-26 12:14:23 +0300

    update main view

--- application/core/View.php
+++ application/core/View.php
@@ -21,14 +21,30 @@ class View{
     }
 
     public function render($title, $vars = []){
-        if(file_exists('application/views/' . $this->path . '.php')){
+        extract($vars);
+        $path = 'application/views/' . $this->path . '.php';
+        if(file_exists($path)){
             ob_start();
-            require_once 'application/views/' . $this->path . '.php';
+            require_once $path;
             $content = ob_get_clean();
             require_once 'application/views/layouts/' . $this->layout . '.php';
-        } else{
-            echo 'view not found: ' . $this->path;
         }
+//          else{
+//            echo 'view not found: ' . $this->path;
+//        }
+    }
+
+    public function redirect($url){
+        header('location: ' . $url);
+    }
+
+    public static function errorCode($code){
+        http_response_code($code);
+        $path = 'application/views/errors/' . $code . '.php';
+        if (file_exists($path)){
+            require_once $path;
+        }
+        exit;
     }
 
 }
